796534df3072fa70b98f1cc364b20123eb019928
kemi-20 <yuminghao805@gmail.com>
2026-03-04 18:10:58 +0800

    modified:   astro.config.mjs 	modified:   src/config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://yumihu-blog.netlify.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
@@ -66,7 +66,7 @@ export default defineConfig({
 			defaultProps: {
 				wrap: true,
 				overridesByLang: {
-					'shellsession': {
+					shellsession: {
 						showLineNumbers: false,
 					},
 				},
